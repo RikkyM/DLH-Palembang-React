514d6a3123a4f4cdf00c244e1d5a4c3ed190db7f
RikkyM <rikky.mahendra54@gmail.com>
2025-10-12 05:26:13 +0700

    add: rekapitulasi penerimaan

--- resources/js/Features/Rekapitulasi/Penerimaan/Index.jsx
+++ resources/js/Features/Rekapitulasi/Penerimaan/Index.jsx
@@ -8,6 +8,7 @@ const Index = ({ datas, filters }) => {
   const [sort, setSort] = useState(filters.sort || null);
   const [direction, setDirection] = useState(filters.direction || null);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasFilter, setHasFilter] = useState(false);
 
   const columns = [
     { key: "id", label: "No", align: "text-center w-10" },
@@ -27,8 +28,8 @@ const Index = ({ datas, filters }) => {
     const params = { ...additionalParams };
 
     // if (perPage && perPage !== 10) params.per_page = perPage;
-    if (startDate) params.tanggal_mulai = startDate;
-    if (endDate) params.tanggal_akhir = endDate;
+    // if (startDate) params.tanggal_mulai = startDate;
+    // if (endDate) params.tanggal_akhir = endDate;
     if (sort && sort !== "id") {
       params.sort = sort;
       if (direction && direction.toLowerCase() === "asc") {
@@ -46,6 +47,8 @@ const Index = ({ datas, filters }) => {
   };
 
   useEffect(() => {
+    if (!hasFilter) return;
+
     setIsLoading(true);
     const timeoutId = setTimeout(() => {
       const params = buildParams();
@@ -69,6 +72,7 @@ const Index = ({ datas, filters }) => {
 
     const params = buildParams();
 
+    setHasFilter(true)
     setIsLoading(true);
     router.get(
       route("super-admin.rekapitulasi.penerimaan"),
@@ -120,7 +124,7 @@ const Index = ({ datas, filters }) => {
                   disabled={isLoading}
                   className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded bg-black px-4 py-2 text-white"
                 >
-                  Proses
+                  Cari
                 </button>
               </div>
             </form>
@@ -130,43 +134,44 @@ const Index = ({ datas, filters }) => {
         <div
           className={`max-h-[calc(100%_-_230px)] overflow-auto rounded sm:max-h-[calc(100%_-_180px)] md:max-h-[calc(100%_-_200px)] lg:max-h-[calc(100%_-_150px)] ${!isLoading && "shadow"}`}
         >
-          {isLoading ? (
-            <div className="mb-2 flex h-16 items-center justify-center gap-2 bg-white px-2 text-sm text-gray-500 shadow">
-              <svg
-                className="h-4 w-4 animate-spin"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                />
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8v8z"
-                />
-              </svg>
-              Memuat data...
-            </div>
-          ) : (
-            <>
-              <table className="min-w-full divide-y divide-gray-300 p-3">
-                <thead className="truncate">
-                  <TableHead
-                    columns={columns}
-                    sort={sort}
-                    direction={direction}
-                    onSort={(column, dir) => {
-                      setSort(column);
-                      setDirection(dir);
-                    }}
-                  >
-                    {/* <th
+          {hasFilter &&
+            (isLoading ? (
+              <div className="mb-2 flex h-16 items-center justify-center gap-2 bg-white px-2 text-sm text-gray-500 shadow">
+                <svg
+                  className="h-4 w-4 animate-spin"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  />
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v8z"
+                  />
+                </svg>
+                Memuat data...
+              </div>
+            ) : (
+              <>
+                <table className="min-w-full divide-y divide-gray-300 p-3">
+                  <thead className="truncate">
+                    <TableHead
+                      columns={columns}
+                      sort={sort}
+                      direction={direction}
+                      onSort={(column, dir) => {
+                        setSort(column);
+                        setDirection(dir);
+                      }}
+                    >
+                      {/* <th
                       colSpan={2}
                       className="sticky top-0 select-none bg-[#F1B174]"
                     >
@@ -178,14 +183,14 @@ const Index = ({ datas, filters }) => {
                     >
                       Approval Keuangan
                     </th> */}
-                    <th className="sticky top-0 select-none bg-[#F1B174] text-left">
-                      Total Bayar
-                    </th>
-                    <th className="sticky top-0 select-none bg-[#F1B174] text-left">
-                      Sisa Bayar
-                    </th>
-                  </TableHead>
-                  {/* <tr className="text-white *:p-2 *:text-xs *:font-medium *:uppercase *:md:text-sm">
+                      <th className="sticky top-0 select-none bg-[#F1B174] text-left">
+                        Total Bayar
+                      </th>
+                      <th className="sticky top-0 select-none bg-[#F1B174] text-left">
+                        Sisa Bayar
+                      </th>
+                    </TableHead>
+                    {/* <tr className="text-white *:p-2 *:text-xs *:font-medium *:uppercase *:md:text-sm">
                     <th className="sticky top-9 select-none bg-[#F1B174]">
                       Total Bayar
                     </th>
@@ -199,78 +204,78 @@ const Index = ({ datas, filters }) => {
                       Sisa Bayar
                     </th>
                   </tr> */}
-                </thead>
-                <tbody>
-                  <tr>
-                    {/* <td>1</td>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      {/* <td>1</td>
                   <td>TEST</td>
                   <td>3</td>
                   <td>10000</td>
                   <td>10000</td>
                   <td>10000</td>
                   <td>10000</td> */}
-                  </tr>
-                  {datas && (datas.data ?? datas)?.length > 0 ? (
-                    (datas.data ?? datas).map((data, i) => (
-                      <tr
-                        key={data.id ?? i}
-                        className={`*:p-2 ${i % 2 === 0 ? "bg-[#B3CEAF]" : "bg-white"}`}
-                        onClick={() => openDetail(data)}
-                      >
-                        {console.log(data)}
-                        <td className="text-left">
-                          <div className="w-10 text-center">
-                            {((datas.current_page ?? 1) - 1) *
-                              (datas.per_page ?? (datas.data ?? datas).length) +
-                              i +
-                              1}
-                          </div>
-                        </td>
-                        <td>{data.namaUptd}</td>
-                        <td>
-                          {new Intl.NumberFormat("id-ID", {
-                            style: "currency",
-                            currency: "IDR",
-                            minimumFractionDigits: 0,
-                          }).format(data.tagihanPertahun ?? 0)}
-                        </td>
-                        <td>
-                          {new Intl.NumberFormat("id-ID", {
-                            style: "currency",
-                            currency: "IDR",
-                            minimumFractionDigits: 0,
-                          }).format(data.totalBayar ?? 0)}
-                        </td>
-                        <td>
-                          {new Intl.NumberFormat("id-ID", {
-                            style: "currency",
-                            currency: "IDR",
-                            minimumFractionDigits: 0,
-                          }).format(
-                            data.tagihanPertahun - data.totalBayar ?? 0,
-                          )}
-                        </td>
-                        {/* <td>{data.namaKategori}</td>
+                    </tr>
+                    {datas && (datas.data ?? datas)?.length > 0 ? (
+                      (datas.data ?? datas).map((data, i) => (
+                        <tr
+                          key={data.id ?? i}
+                          className={`*:p-2 ${i % 2 === 0 ? "bg-[#B3CEAF]" : "bg-white"}`}
+                          onClick={() => openDetail(data)}
+                        >
+                          {console.log(data)}
+                          <td className="text-left">
+                            <div className="w-10 text-center">
+                              {((datas.current_page ?? 1) - 1) *
+                                (datas.per_page ??
+                                  (datas.data ?? datas).length) +
+                                i +
+                                1}
+                            </div>
+                          </td>
+                          <td>{data.namaUptd}</td>
+                          <td>
+                            {new Intl.NumberFormat("id-ID", {
+                              style: "currency",
+                              currency: "IDR",
+                              minimumFractionDigits: 0,
+                            }).format(data.tagihanPertahun) ?? 0}
+                          </td>
+                          <td>
+                            {new Intl.NumberFormat("id-ID", {
+                              style: "currency",
+                              currency: "IDR",
+                              minimumFractionDigits: 0,
+                            }).format(data.totalBayar) ?? 0}
+                          </td>
+                          <td>
+                            {new Intl.NumberFormat("id-ID", {
+                              style: "currency",
+                              currency: "IDR",
+                              minimumFractionDigits: 0,
+                            }).format(data.tagihanPertahun - data.totalBayar) ??
+                              0}
+                          </td>
+                          {/* <td>{data.namaKategori}</td>
                         <td>{data.namaSubKategori}</td>
                         <td className="text-center">{data.jumlah}</td> */}
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td
+                          colSpan="4"
+                          className="py-8 text-center text-xs text-gray-500 lg:text-sm"
+                        >
+                          {startDate && endDate
+                            ? "Tidak ada SPKRD pada waktu tersebut"
+                            : "Belum ada data wajib retribusi"}
+                        </td>
                       </tr>
-                    ))
-                  ) : (
-                    <tr>
-                      <td
-                        colSpan="4"
-                        className="py-8 text-center text-xs text-gray-500 lg:text-sm"
-                      >
-                        {startDate && endDate
-                          ? "Tidak ada SPKRD pada waktu tersebut"
-                          : "Belum ada data wajib retribusi"}
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
-            </>
-          )}
+                    )}
+                  </tbody>
+                </table>
+              </>
+            ))}
         </div>
       </section>
     </>
