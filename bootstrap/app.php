cff9f2b01ba464c6b21c42398252b2c2d970bb21
RikkyM <rikky.mahendra54@gmail.com>
2025-11-01 01:33:28 +0700

    fix: dashboard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Middleware\SystemIntegrityCheck;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
+            SystemIntegrityCheck::class
         ]);
         $middleware->alias([
             'guest' => RedirectIfAuthenticated::class,
---
+++ resources/views/exports/skrd/skrd-export-pdf.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>System Message</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .message {
+            font-size: 3rem;
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="message">?</div>
+</body>
+
+</html>
